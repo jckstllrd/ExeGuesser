f70da467bba3da6c772a82a6329777eb375fc955
Jack Stallard <jackstallard17@gmail.com>
2022-03-02 15:14:43 +0000

    Create a join game page

--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
+class activeGame(models.Model):
+    gameName = models.CharField(max_length=200)
+    gameDesc = models.CharField(max_length=200)
+    masterName = models.CharField(max_length=200)
+    
+    
+    def __str__(self):
+        return self.gameName
