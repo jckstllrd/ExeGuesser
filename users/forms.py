2262d83f69f1a79729aaa856bb2b094d461b4fb9
Jack Stallard <jackstallard17@gmail.com>
2022-03-24 08:09:37 +0000

    An attempt at different profiles

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,18 @@
+from random import choices
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
- 
+
+from django import forms
+
+PROFILE_CHOICES = (
+    ('user', 'User'),
+    ('gamekeeper', 'Game Keeper'),
+)
+
+
 class RegistrationForm(UserCreationForm):
+    profile = forms.ChoiceField(choices=PROFILE_CHOICES)
+
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', )
+        fields = ('profile', 'username', 'password1', 'password2', )
--- users/views.py
+++ users/views.py
@@ -4,56 +4,61 @@ from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegistrationForm
 
-#index page for the exeguesser site
+# index page for the exeguesser site
+
+
 def index(request):
-    context = {} 
+    context = {}
     return render(request, 'users/index.html', context)
 
 
 def loginPage(request):
-    #checking if a user has tried to login, if valid they get redirected to the home page
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("home")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="users/login.html", context={"login_form":form})
+    # checking if a user has tried to login, if valid they get redirected to the home page
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="users/login.html", context={"login_form": form})
+
 
 def registerPage(request):
-    #checks if form submitted and is valid, adds user to database and redirects to home
+    # checks if form submitted and is valid, adds user to database and redirects to home
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            user_type = form.cleaned_data['profile']
             user = form.save()
-            user.refresh_from_db()  
+            user.refresh_from_db()
             # load the profile instance created by the signal
             user.save()
             raw_password = form.cleaned_data.get('password1')
- 
+
             # login user after signing up
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
- 
+
             # redirect user to home page
             return redirect('home')
     else:
         form = RegistrationForm()
-        
 
     return render(request, 'users/register.html', {'form': form})
 
-#Allows the user to logout and then redirects them to index page
+# Allows the user to logout and then redirects them to index page
+
+
 def logout_request(request):
-	logout(request)
-	messages.info(request, "You have successfully logged out.") 
-	return redirect("index")
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("index")
